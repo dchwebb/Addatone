9cd5428ffb952114271e57ab92fb4ed89dfc1365
Dominic Webb <dwebb@reapit.com>
2020-03-11 17:20:57 +0000

    Performance tweaks to offset mechanism; refactoring

--- Addatone_MachX03/src/Sample_Position.v
+++ Addatone_MachX03/src/Sample_Position.v
@@ -1,19 +1,19 @@
 /*
-	Module is responsible for supplying the sample value of each harmonic as incremented by the top module.
-	This involves keeping track of the position of each harmonic's current sample in RAM (SamplePos_RAM module),
+	Module is responsible for supplying the sample value of each harmonic.
+	This involves keeping track of the position of each harmonic's current sample position in RAM (SamplePos_RAM module),
 	incrementing and adding the current frequency to the sample position, then looking up the sample value in the Sine LUT.
 	It also applies a frequency offset so higher harmonics can be increasingly detuned.
-	This works in two directions - odd harmonics are tuned down and even harmonics tuned up.
+	This works in two directions - odd harmonics are tuned up and even harmonics tuned down.
 */
 module Sample_Position
 	(
 		input wire i_Reset,
 		input wire i_Clock,
 		input wire [15:0] i_Frequency,
-		input wire [15:0] i_Freq_Scale,			// Offset to allow cumulative frequency shifting of higher harmonics
+		input wire [15:0] i_Freq_Offset,			// Offset to allow cumulative frequency shifting of higher harmonics
 		input wire [7:0] i_Harmonic,
-		output reg o_Sample_Ready,					// Tells top module that sample position has been loaded
-		input wire i_Next_Sample,					// Trigger from top module to say current value has been read and ready for next sample
+		output reg o_Sample_Ready,						// Tells top module that sample position has been loaded
+		input wire i_Next_Sample,						// Trigger from top module to say current value has been read and ready for next sample
 		output wire [15:0] o_Sample_Value,
 		output reg o_Freq_Too_High
 	);
@@ -22,7 +22,7 @@ module Sample_Position
 	wire [15:0] Sample_Pos_Read;
 	reg [15:0] Sample_Position;
 	reg [10:0] LUT_Pos;
-
+	
 	// Initialise Sample Position RAM (IP)
 	SamplePos_RAM sample_pos_ram(.Clock(i_Clock), .ClockEn(1'b1), .Reset(i_Reset), .WE(Sample_Pos_WE), .Address(i_Harmonic), .Data(Sample_Position), .Q(Sample_Pos_Read));
 
@@ -30,14 +30,15 @@ module Sample_Position
 	SineLUT sin_lut (.Address(LUT_Pos), .OutClock(i_Clock), .OutClockEn(1'b1), .Reset(i_Reset), .Q(o_Sample_Value));
 
 	reg [15:0] Accumulated_Frequency;
-	reg [15:0] Accumulated_Freq_Scale;
-	reg [2:0] State_Machine;
+	reg [15:0] Accumulated_Freq_Offset;
+	reg signed [15:0] Accumulated_Offset;
+	reg [2:0] SM_Sample_Position;
 	localparam [2:0] sm_init = 3'd0;
 	localparam [2:0] sm_sample_pos = 3'd1;
 	localparam [2:0] sm_sine_lut = 3'd2;
-	localparam [2:0] sm_waiting = 3'd3;
-	localparam [2:0] sm_offset = 3'd4;
-	localparam [2:0] sm_offset2 = 3'd5;
+	localparam [2:0] sm_offset = 3'd3;
+	localparam [2:0] sm_waiting = 3'd4;
+
 	
 	always @(posedge i_Clock) begin
 		if (i_Reset) begin
@@ -46,18 +47,18 @@ module Sample_Position
 			Sample_Position <= 1'b0;
 			o_Freq_Too_High <= 1'b0;
 			LUT_Pos <= 1'b0;
-			State_Machine <= sm_init;
+			SM_Sample_Position <= sm_init;
 		end
 		else begin
-			case (State_Machine)
+			case (SM_Sample_Position)
 				sm_init:
 					begin
 						Accumulated_Frequency <= i_Frequency;
-						Accumulated_Freq_Scale <= 1'b0;
+						Accumulated_Freq_Offset <= 1'b0;
 						o_Sample_Ready <= 1'b0;
 						Sample_Pos_WE <= 1'b0;
 						o_Freq_Too_High <= 1'b0;
-						State_Machine <= sm_sample_pos;
+						SM_Sample_Position <= sm_sample_pos;
 					end
 					
 				sm_sample_pos:
@@ -65,7 +66,7 @@ module Sample_Position
 						// increment next sample position by frequency: number of items in sine LUT is 2048 (32*2048=65536) which means that we can divide by 32 to get correct position
 						Sample_Position <= Sample_Pos_Read + Accumulated_Frequency;
 						Sample_Pos_WE <= 1'b1;
-						State_Machine <= sm_sine_lut;
+						SM_Sample_Position <= sm_sine_lut;
 					end
 
 				sm_sine_lut:
@@ -74,41 +75,37 @@ module Sample_Position
 						o_Sample_Ready <= 1'b1;
 						Sample_Pos_WE <= 1'b0;
 						LUT_Pos <= Sample_Position[15:5];
-						State_Machine <= sm_waiting;
+						Accumulated_Offset <= 1'b0;
+						SM_Sample_Position <= sm_offset;
+						Accumulated_Frequency <= Accumulated_Frequency + i_Frequency;
 					end
 					
-				sm_waiting:
-					// Once sample has been read by top restart
+				sm_offset:
+					// Apply frequency offset adjustment, checking that when reducing frequency we are not reducing below zero
 					begin
-						if (Accumulated_Frequency > 20000) begin
+						if (i_Harmonic[0])
+							Accumulated_Offset <= Accumulated_Freq_Offset;
+						else if (Accumulated_Freq_Offset < Accumulated_Frequency)
+							Accumulated_Offset <= -Accumulated_Freq_Offset;
+
+						SM_Sample_Position <= sm_waiting;
+					end
+
+				sm_waiting:
+					// Once sample has been read by top adjust accumulated frequency registers and restart
+					begin
+						if (Accumulated_Frequency > 20000) begin		// If upper level of frequency is beyond audible frequency tell top to ignore remaining harmonics
 							o_Freq_Too_High <= 1'b1;
 						end
 						
 						if (i_Next_Sample) begin
-							Accumulated_Frequency <= Accumulated_Frequency + i_Frequency;
+							Accumulated_Frequency <= Accumulated_Frequency + Accumulated_Offset;
+							Accumulated_Freq_Offset <= Accumulated_Freq_Offset + i_Freq_Offset;
 							o_Sample_Ready <= 1'b0;
-							State_Machine <= sm_offset;
+							SM_Sample_Position <= (i_Harmonic == 1'b0) ? sm_init : sm_sample_pos;
 						end
 					end
 				
-				sm_offset:
-					begin
-						State_Machine <= (i_Harmonic == 1'b0) ? sm_init : sm_sample_pos;
-						if (i_Harmonic[0]) begin
-							Accumulated_Freq_Scale <= Accumulated_Freq_Scale + i_Freq_Scale;
-							Accumulated_Frequency <= Accumulated_Frequency + Accumulated_Freq_Scale;
-						end
-						else if (Accumulated_Freq_Scale < Accumulated_Frequency)
-							State_Machine <= sm_offset2;
-					end
-
-				sm_offset2:
-					begin
-						//Accumulated_Freq_Scale <= Accumulated_Freq_Scale + i_Freq_Scale;
-						Accumulated_Frequency <= Accumulated_Frequency - Accumulated_Freq_Scale;
-						State_Machine <= (i_Harmonic == 1'b0) ? sm_init : sm_sample_pos;
-					end
-
 			endcase
 		end
 		
--- Addatone_MachX03/src/top.v
+++ Addatone_MachX03/src/top.v
@@ -18,57 +18,50 @@ module top
 	assign reset = ~rstn;
 
 	//	Initialise 72MHz PLL clock from dev board 12 MHz crystal (dev board pin C8)
-	wire fpga_clock;
-	OscPll pll(.CLKI(crystal_osc), .CLKOP(fpga_clock));
+	wire Main_Clock;
+	OscPll pll(.CLKI(crystal_osc), .CLKOP(Main_Clock));
 
 	// Sample position RAM - memory array to store current position in cycle of each harmonic
-	reg [7:0] harmonic = 1'b0;						// Number of harmonic being calculated
-	reg next_sample;										// Trigger from top module to say current value has been read and ready for next sample
-	wire signed [15:0] sample_value;
-	reg [15:0] Frequency = 16'd50;
-	reg [15:0] Freq_Scale = 16'd120;
-	wire sample_ready, Freq_Too_High;
-	Sample_Position sample_position(.i_Reset(reset), .i_Clock(fpga_clock), .i_Frequency(Frequency), .i_Freq_Scale(Freq_Scale), .i_Harmonic(harmonic), .o_Sample_Ready(sample_ready), .i_Next_Sample(next_sample), .o_Sample_Value(sample_value), .o_Freq_Too_High(Freq_Too_High));
-
-	// Initialise ADC SPI input microcontroller
-	wire [15:0] adc_data0;
-	wire [15:0] adc_data1;
-	wire [15:0] adc_data2;
-	wire [15:0] adc_data3;
-	wire adc_data_received;
-	ADC_SPI_In adc(.i_reset(reset), .i_clock(fpga_clock), .i_SPI_CS(adc_cs), .i_SPI_clock(adc_clock), .i_SPI_data(adc_data), .o_data0(adc_data0), .o_data1(adc_data1), .o_data2(adc_data2), .o_data3(adc_data3), .o_data_received(adc_data_received));
-
-	// Output settings
-	reg signed [31:0] output_sample;
-	reg [15:0] dac_sample;								// Contains output sample scaled
-
-	// Timing and Sine LUT settings
-	reg [15:0] sample_timer = 1'b0;					// Counts up to SAMPLEINTERVAL to set sample rate interval
 	parameter SAMPLERATE = 16'd48000;
 	parameter SAMPLEINTERVAL = 16'd1500;			// Clock frequency / sample rate - eg 88.67Mhz / 44khz = 2015 OR 72MHz / 48kHz = 1500
 
+	reg [15:0] Sample_Timer = 1'b0;					// Counts up to SAMPLEINTERVAL to set sample rate interval
+	reg [7:0] Harmonic = 1'b0;						// Number of harmonic being calculated
+	reg Next_Sample;										// Trigger from top module to say current value has been read and ready for next sample
+	wire signed [15:0] Sample_Value;
+	reg [15:0] Frequency = 16'd50;
+	reg [15:0] Freq_Scale = 16'd120;
+	wire Sample_Ready, Freq_Too_High;
+	Sample_Position sample_position(.i_Reset(reset), .i_Clock(Main_Clock), .i_Frequency(Frequency), .i_Freq_Offset(Freq_Scale), .i_Harmonic(Harmonic), .o_Sample_Ready(Sample_Ready), .i_Next_Sample(Next_Sample), .o_Sample_Value(Sample_Value), .o_Freq_Too_High(Freq_Too_High));
+
+	// Initialise ADC SPI input microcontroller
+	wire [15:0] ADC_Data[3:0];
+	wire ADC_Data_Received;
+	ADC_SPI_In adc(.i_reset(reset), .i_clock(Main_Clock), .i_SPI_CS(adc_cs), .i_SPI_clock(adc_clock), .i_SPI_data(adc_data), .o_data0(ADC_Data[0]), .o_data1(ADC_Data[1]), .o_data2(ADC_Data[2]), .o_data3(ADC_Data[3]), .o_data_received(ADC_Data_Received));
+
 	// Instantiate scaling adder - this scales then accumulates samples for each sine wave
 	parameter DIV_BIT = 9;								// Allows fractions from 1/128 to 127/128 (for DIV_BIT = 7)
-	reg adder1_start, adder2_start, adder_clear;
+	reg Adder1_Start, Adder2_Start, Adder_Clear;
 	reg [DIV_BIT - 1:0] harmonic_scale = 270;	// Level at which higher harmonics are attenuated
 	reg [DIV_BIT - 1:0] scale_initial = 511;
-	wire adder1_ready, adder2_ready;
+	wire Adder1_Ready, Adder2_Ready;
 	wire [DIV_BIT - 1:0] adder_mult;
-	wire signed [31:0] adder1_total;
-	wire signed [31:0] adder2_total;
-	reg signed [31:0] r_adder1_total;
-	reg signed [31:0] r_adder2_total;
-	Adder #(.DIVISOR_BITS(DIV_BIT)) addSample1 (.clock(fpga_clock), .reset(reset), .start(adder1_start), .clear_accumulator(adder_clear),  .multiple(adder_mult), .i_sample(sample_value), .accumulator(adder1_total), .done(adder1_ready));
-	Adder #(.DIVISOR_BITS(DIV_BIT)) addSample2 (.clock(fpga_clock), .reset(reset), .start(adder2_start), .clear_accumulator(adder_clear),  .multiple(adder_mult), .i_sample(sample_value), .accumulator(adder2_total), .done(adder2_ready));
+	wire signed [31:0] Adder1_Total;
+	wire signed [31:0] Adder2_Total;
+	reg signed [31:0] r_Adder1_Total;
+	reg signed [31:0] r_Adder2_Total;
+	Adder #(.DIVISOR_BITS(DIV_BIT)) addSample1 (.clock(Main_Clock), .reset(reset), .start(Adder1_Start), .clear_accumulator(Adder_Clear), .multiple(adder_mult), .i_sample(Sample_Value), .accumulator(Adder1_Total), .done(Adder1_Ready));
+	Adder #(.DIVISOR_BITS(DIV_BIT)) addSample2 (.clock(Main_Clock), .reset(reset), .start(Adder2_Start), .clear_accumulator(Adder_Clear), .multiple(adder_mult), .i_sample(Sample_Value), .accumulator(Adder2_Total), .done(Adder2_Ready));
 
 	// instantiate multiple scaler - this takes incoming ADC reading and uses it to reduce the level of harmonics scaled by the Adder
 	reg start_mult_scaler;
 	reg reset_mult_scaler;
-	scale_mult #(.DIV_BIT(DIV_BIT)) mult (.i_clock(fpga_clock), .i_start(start_mult_scaler), .i_restart(reset_mult_scaler), .i_scale(harmonic_scale), .i_initial(scale_initial), .o_mult(adder_mult));
+	scale_mult #(.DIV_BIT(DIV_BIT)) mult (.i_clock(Main_Clock), .i_start(start_mult_scaler), .i_restart(reset_mult_scaler), .i_scale(harmonic_scale), .i_initial(scale_initial), .o_mult(adder_mult));
 
 	// Instantiate Sample Output module which scales output and sends to DAC
 	reg dac_send;
-	Sample_Output sample_output(.i_Clock(fpga_clock), .i_Reset(reset), .i_Start(dac_send), .i_Sample_L(r_adder1_total), .i_Sample_R(r_adder2_total),.o_SPI_CS(dac_cs), .o_SPI_clock(dac_clock), .o_SPI_data(dac_bit));
+	reg signed [31:0] Output_Sample;
+	Sample_Output sample_output(.i_Clock(Main_Clock), .i_Reset(reset), .i_Start(dac_send), .i_Sample_L(r_Adder1_Total), .i_Sample_R(r_Adder2_Total),.o_SPI_CS(dac_cs), .o_SPI_clock(dac_clock), .o_SPI_data(dac_bit));
 
 	// State Machine settings - used to control calculation of amplitude of each harmonic sample
 	reg [3:0] state_machine;
@@ -83,45 +76,45 @@ module top
 	localparam sm_ready_to_send = 4'd8;
 	localparam sm_cleanup = 4'd9;
 
-	always @(posedge adc_data_received) begin
-		Frequency <= adc_data0;
-		//harmonic_scale <= adc_data1[DIV_BIT - 1:0];		// bottom 8 bits are scale value
-		//scale_initial <= adc_data1[15:DIV_BIT];				// top 8 bits are starting value for scaling (lower if there are more harmonics)
-		harmonic_scale <= adc_data1;
-		scale_initial <= adc_data2;								// Starting value for scaling (lower if there are more harmonics)
-		Freq_Scale <= adc_data3;									// Frequency scaling offset - higher frequencies will be moved further from multiple of fundamental
-		//debug_out <= ~debug_out;
+	always @(posedge ADC_Data_Received) begin
+		Frequency <= ADC_Data[0];
+		//harmonic_scale <= adc_data1[DIV_BIT - 1:0];			// bottom 8 bits are scale value
+		//scale_initial <= adc_data1[15:DIV_BIT];					// top 8 bits are starting value for scaling (lower if there are more harmonics)
+		harmonic_scale <= ADC_Data[1][DIV_BIT - 1:0];			// Rate of attenuation of harmonic scaling (lower means more harmonics)
+		scale_initial <= ADC_Data[2][DIV_BIT - 1:0];								// Starting value for scaling (lower if there are more harmonics)
+		Freq_Scale <= ADC_Data[3];									// Frequency scaling offset - higher frequencies will be moved further from multiple of fundamental
+		debug_out <= ~debug_out;
 	end
 
 	assign err_out = dac_send;
 	
-	always @(posedge fpga_clock) begin
+	always @(posedge Main_Clock) begin
 		if (reset) begin
-			sample_timer <= 1'b0;
+			Sample_Timer <= 1'b0;
 			dac_send <= 1'b0;
-			adder1_start <= 1'b0;
-			adder2_start <= 1'b0;
-			harmonic <= 8'b0;
+			Adder1_Start <= 1'b0;
+			Adder2_Start <= 1'b0;
+			Harmonic <= 8'b0;
 			state_machine <= sm_init;
 		end
 		else begin
-			sample_timer <= sample_timer + 1'b1;
+			Sample_Timer <= Sample_Timer + 1'b1;
 
 			case (state_machine)
 				sm_init:
 				begin
 					dac_send <= 1'b0;
-					next_sample <= 1'b0;
-					adder_clear <= 1'b0;
+					Next_Sample <= 1'b0;
+					Adder_Clear <= 1'b0;
 					reset_mult_scaler <= 1'b0;
 					state_machine <= sm_adder_start;
 				end
 
 				sm_adder_mult:
 				begin
-					next_sample <= 1'b0;
-					adder1_start <= 1'b0;
-					adder2_start <= 1'b0;
+					Next_Sample <= 1'b0;
+					Adder1_Start <= 1'b0;
+					Adder2_Start <= 1'b0;
 
 					// decrease harmonic scaler
 					start_mult_scaler <= 1'b1;
@@ -133,62 +126,60 @@ module top
 					start_mult_scaler <= 1'b0;
 
 					// Wait until the sample value is ready and Adder is free and then start the next calculation
-					if (sample_ready && (harmonic[0] == 1'b0 ? adder1_ready : adder2_ready)) begin
-					//if (sample_ready && adder1_ready) begin
-						harmonic <= harmonic + 2'b1;								// Load up next sample position
-						next_sample <= 1'b1;
-						adder1_start <= 1'b1;
+					if (Sample_Ready && (Harmonic[0] == 1'b0 ? Adder1_Ready : Adder2_Ready)) begin
+					//if (Sample_Ready && Adder1_Ready) begin
+						Harmonic <= Harmonic + 2'b1;								// Load up next sample position
+						Next_Sample <= 1'b1;
+						Adder1_Start <= 1'b1;
 
-						if (harmonic[0] == 1'b0)		// even harmonics
-							adder1_start <= 1'b1;											// Tell the adder the next sample is ready
+						if (Harmonic[0] == 1'b0)		// even harmonics
+							Adder1_Start <= 1'b1;											// Tell the adder the next sample is ready
 						else
-							adder2_start <= 1'b1;
-						state_machine <= (harmonic >= NO_OF_HARMONICS || Freq_Too_High) ? sm_adder_wait : sm_adder_mult;
+							Adder2_Start <= 1'b1;
+						state_machine <= (Harmonic >= NO_OF_HARMONICS || Freq_Too_High) ? sm_adder_wait : sm_adder_mult;
 					end
 				end
 
 				sm_adder_wait:
 				begin
-					next_sample <= 1'b0;
-					adder1_start <= 1'b0;
-					adder2_start <= 1'b0;
-					state_machine <= sm_calc_done;								// Pause to let adder clear adder_ready state
+					Next_Sample <= 1'b0;
+					Adder1_Start <= 1'b0;
+					Adder2_Start <= 1'b0;
+					state_machine <= sm_calc_done;								// Pause to let adder clear adder_Ready state
 				end
 
 				sm_calc_done:
 				begin
-					if (adder1_ready && adder2_ready) begin
+					if (Adder1_Ready && Adder2_Ready) begin
 						// all harmonics calculated - offset output for sending to DAC
-						//output_sample <= 32'h31000 + adder2_total;			// Add extra 2^18 (approx) to cancel divide by 4 on final value
-						r_adder1_total <= adder1_total;
-						r_adder2_total <= adder2_total;
+						//Output_Sample <= 32'h31000 + Adder2_Total;			// Add extra 2^18 (approx) to cancel divide by 4 on final value
+						r_Adder1_Total <= Adder1_Total;
+						r_Adder2_Total <= Adder2_Total;
 						state_machine <= sm_scale_sample;
 					end
 				end
 
 				sm_scale_sample:
 				begin
-					//dac_sample <= output_sample >> 3;							// scale output sample to send to DAC
-					adder_clear <= 1'b1;
+					Adder_Clear <= 1'b1;
 					state_machine <= sm_cleanup;
 				end
 				
 				sm_cleanup:
 					begin
-						adder_clear <= 1'b0;
+						Adder_Clear <= 1'b0;
 						state_machine <= sm_ready_to_send;
 					end
 					
 				sm_ready_to_send:
-					if (sample_timer == SAMPLEINTERVAL) begin
+					if (Sample_Timer == SAMPLEINTERVAL) begin
 						// Send sample value to DAC
 						dac_send <= 1'b1;
 
 						// Clean state ready for next loop
-						sample_timer <= 1'b0;
-						harmonic <= 8'b0;
-						next_sample <= 1'b1;
-						//adder_clear <= 1'b0;
+						Sample_Timer <= 1'b0;
+						Harmonic <= 8'b0;
+						Next_Sample <= 1'b1;
 						
 						reset_mult_scaler <= 1'b1;
 						state_machine <=  sm_init;
