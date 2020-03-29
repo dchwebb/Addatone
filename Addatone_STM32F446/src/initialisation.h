69e85ed6529ff76e0fdedfad3e1bf609896a0648
Dominic Webb <dwebb@reapit.com>
2020-03-29 15:20:05 +0100

    Added STM32F446 project to test programming FPGA from microcontroller

---
+++ Addatone_STM32F446/src/initialisation.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "stm32f4xx.h"
+#include <algorithm>
+#include <cmath>
+
+extern volatile uint32_t SysTickVal;
+
+#define ADC_BUFFER_LENGTH 4
+extern volatile uint16_t ADC_array[ADC_BUFFER_LENGTH * 4];
+
+
+void SystemClock_Config(void);
+void InitADC(void);
+void InitIO(void);
+void InitSysTick();
+void InitCoverageTimer();
+
+void InitSPI();
+void InitSPITimer();
+void sendSPIData(uint16_t data);
+void clearSPI();
+
+void InitI2S();
+void sendI2SData(uint32_t data);
